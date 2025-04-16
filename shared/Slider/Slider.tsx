77232c6be17a7e84da910aa264732807ac6224bb
Dominic <dominicezeifejafor@gmail.com>
2025-04-16 02:00:33 +0100

    added active state for pages, width of components, made the slider longer

--- shared/Slider/Slider.tsx
+++ shared/Slider/Slider.tsx
@@ -1,21 +1,56 @@
 import React from 'react';
 import Image from 'next/image';
 import styles from './Slider.module.scss'
+import { slidesArray } from '@/mock/navLists.mock';
 
 const Slider = () => {
+  
   return (
     <div className={styles.slides}>
-        {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(item => (
-            <div key={item} className={styles.slide}>
-                <Image
-                    quality={100}
-                    priority
-                    src="/images/slide.png"
-                    alt="Govtech"
-                    fill
-                />
-            </div>
-        ))}
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item: number) => (
+        <div key={item} className={styles.slide}>
+          <Image
+            quality={100}
+            priority
+            src="/images/slide.png"
+            alt="Govtech"
+            fill
+          />
+        </div>
+      ))}
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item: number) => (
+        <div key={item} className={styles.slide}>
+          <Image
+            quality={100}
+            priority
+            src="/images/slide.png"
+            alt="Govtech"
+            fill
+          />
+        </div>
+      ))}
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item: number) => (
+        <div key={item} className={styles.slide}>
+          <Image
+            quality={100}
+            priority
+            src="/images/slide.png"
+            alt="Govtech"
+            fill
+          />
+        </div>
+      ))}
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item: number) => (
+        <div key={item} className={styles.slide}>
+          <Image
+            quality={100}
+            priority
+            src="/images/slide.png"
+            alt="Govtech"
+            fill
+          />
+        </div>
+      ))}
     </div>
   )
 }
