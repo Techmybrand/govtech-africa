776f525fd802831038050ed9e7e1308d2f3e0610
Dominic <dominicezeifejafor@gmail.com>
2025-04-18 13:39:03 +0100

    UI corrections

--- Views/AboutUsView/AboutUsView.tsx
+++ Views/AboutUsView/AboutUsView.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import { ConferenceHighlights, CrossRoads, Participation, Partners, Registration } from '@/components';
 import { BookATicket, HeroComponent, Slider } from '@/shared';
+import styles from './AboutUsView.module.scss';
 
 const AboutUsView = () => {
   return (
     <>
       <HeroComponent title={`Hosted for Africa, by Africans`}
-        subText={`A digital public sector for Africa is no longer optional—-it is the lever 
-          of governance that serves, empowers, and transforms.
-          At the Africa Govtech Week, we are bent on our mission to drive the 
-          digital transformation of Africa's public sector.
-        `}
+        subText={
+          <div className={styles.hero_subtext}>
+            <p className={styles.subtext_lg}>
+              {`A digital public sector for Africa is no longer optional—-it is the lever 
+              of governance that serves, empowers, and transforms.
+              At the Africa Govtech Week, we are bent on our mission to drive the 
+              digital transformation of Africa's public sector.`}
+            </p>
+            <p className={styles.subtext_mobile}>
+              {`A digital public sector for Africa is no longer optional—-it is the lever 
+              of governance that serves, empowers, and transforms.`}
+              <br />
+              <br />
+              {`At the Africa Govtech Week, we are bent on our mission to drive the 
+              digital transformation of Africa's public sector.`}
+            </p>
+          </div>
+        }
         backgroundImage='/images/hero-2.jpeg'
+        headerSubtextClass={styles.header_subtext_class}
+        headerTitleClass={styles.header_title_class}
       />
       <BookATicket />
       <CrossRoads />
